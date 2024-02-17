630e7ca35676ea11fe7c3d32fbaba9fd1572bc9e
simon-s-99 <142667693+simon-s-99@users.noreply.github.com>
2024-02-17 14:42:56 +0100

    changed order to be chronologically accurate
    + added comments
    
    + added classlist for an incomplete task aswell, this will make it easier to identify done/not done tasks later on in the code

--- scripts/todo.js
+++ scripts/todo.js
@@ -1,21 +1,29 @@
 export function addTodo(todo) {
-    const main = document.getElementById("main");
+    // this function adds a div holding everything related to 
+    // one "todo" post in the application 
     const todoContainer = document.createElement("div");
-
+    
+    const todoLabel = document.createElement("label");
+    todoLabel.textContent = todo;
+    
     const todoInput = document.createElement("input");
     todoInput.type = "checkbox";
     todoInput.checked = false;
-
-    const todoLabel = document.createElement("label");
-    todoLabel.textContent = todo;
-
+    todoInput.classList.add("unchecked");
+    
+    // toggle a task as complete/incomplete
     todoInput.onchange = () => {
         if (todoInput.checked) {
+            todoInput.classList.remove("unchecked");
             todoInput.classList.add("checked");
         } else {
-            todoInput.classList.remove("checked")
+            todoInput.classList.remove("checked");
+            todoInput.classList.add("unchecked");
         }
     }
+    
     todoContainer.append(todoInput, todoLabel);
+
+    const main = document.getElementById("main");
     main.append(todoContainer);
 }
