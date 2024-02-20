aa767fa62e969c8ac0ca3396880042522fc52a94
Adam Kumlin <skola.adamkumlin99@gmail.com>
2024-02-20 13:53:21 +0100

    add tests
    Co-Authored-By: Simon Sörqvist <simon.t.sorqvist@gmail.com>

--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,7 @@ const form = document.getElementById("form");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  let term = document.getElementById("search");
+  let term = document.getElementById("todo-textbox");
   if (term.value !== "") { // to prevent empty todos in the list 
     addTodo(term.value);
     term.value = "";
---
+++ tests/tests.spec.js
@@ -0,0 +1,43 @@
+// @ts-check
+const { test, expect } = require("@playwright/test");
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("http://127.0.0.1:5500");
+});
+
+test("add one todo", async ({ page }) => {
+  const todoInput = page.locator("#todo-textbox");
+  await todoInput.fill("Buy milk");
+  await todoInput.press("Enter");
+
+  await expect(page.getByText("Buy milk")).toBeVisible();
+});
+
+test("zero items left", async ({ page }) => {
+  const todoInput = page.locator("#todo-textbox");
+  await todoInput.fill("Buy milk");
+  await todoInput.press("Enter");
+
+  await expect(page.getByText("1 items left")).toBeVisible();
+
+  const todoToggle = page.locator(".unchecked");
+  todoToggle.click();
+
+  await expect(page.getByText("0 items left")).toBeVisible();
+});
+
+test("two items left", async ({ page }) => {
+  const todoInput = page.locator("#todo-textbox");
+  await todoInput.fill("Buy milk");
+  await todoInput.press("Enter");
+  await todoInput.fill("Buy peanuts");
+  await todoInput.press("Enter");
+  await todoInput.fill("Feed red panda");
+  await todoInput.press("Enter");
+
+  //const addPeanutsToggle = page.getByText("Buy peanuts");
+  const todoToggle = page.locator("main div:first-child div input");
+  todoToggle.click();
+
+  await expect(page.getByText("2 items left")).toBeVisible();
+});
